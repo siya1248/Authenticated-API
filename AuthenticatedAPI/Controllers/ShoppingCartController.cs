b42f56ad3547862ab6f8065e997a45cefdfb7776
siya1248 <71254522+siya1248@users.noreply.github.com>
2024-02-23 23:18:36 +0000

    Added a get end point

--- AuthenticatedAPI/Controllers/ShoppingCartController.cs
+++ AuthenticatedAPI/Controllers/ShoppingCartController.cs
@@ -7,6 +7,22 @@ namespace AuthenticatedAPI.Controllers
     [Route("api/[controller]")]
     public class ShoppingCartController : ControllerBase
     {
-        
+        private readonly SecurityContext _context'
+        public ShoppingCartController(SecurityContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var userId = User.Identity.Name;
+
+            var shoppingCartItems = _context.shoppingCarts
+            .Where(cart => cart.User == userId)
+            .SelectMany(cart => cart.products)
+            .ToList();
+
+            return Ok(shoppingCartItems);
+        }
     }
 }
