b058269ab0e2cffb884536179a44b9a6d0a19f4b
siya1248 <71254522+siya1248@users.noreply.github.com>
2024-02-23 23:02:03 +0000

    Created shopping cart controller

---
+++ AuthenticatedAPI/Controllers/ShoppingCartController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+namespace AuthenticatedAPI.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ShoppingCartController : ControllerBase
+    {
+        
+    }
+}
