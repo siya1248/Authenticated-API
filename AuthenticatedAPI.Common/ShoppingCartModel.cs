4c8211239e5a3ea235943bd85853a4cf4fe69ada
siya1248 <71254522+siya1248@users.noreply.github.com>
2024-02-23 21:19:05 +0000

    Created shopping cart model

---
+++ AuthenticatedAPI.Common/ShoppingCartModel.cs
@@ -0,0 +1,13 @@
+﻿namespace AuthenticatedAPI.Common;
+
+public class ShoppingCartModel
+{
+    public int ID {get; set;}
+    public string User {get; set;}
+    public List<ProductModel> products {get; set;}
+
+    public ShoppingCartModel()
+    {
+        products = new List<ProductModel>();
+    }
+}
